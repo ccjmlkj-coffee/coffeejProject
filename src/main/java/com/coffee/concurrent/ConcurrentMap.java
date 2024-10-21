ae21833d3ea246ee97d883a881221722205a53a7
makaijian <makaijian@huya.com>
2024-10-22 00:13:34 +0800

    hashMap

--- src/main/java/com/coffee/concurrent/ConcurrentMap.java
+++ src/main/java/com/coffee/concurrent/ConcurrentMap.java
@@ -1,7 +1,19 @@
 package com.coffee.concurrent;
 
+import com.coffee.util.NumberUtils;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentMap {
     ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+    static final int HASH_BITS = 0x7fffffff; // usable bits of normal node hash
+    public static void main(String[] args) {
+        String key = "makaijian";
+        int h = key.hashCode();
+        System.out.println(h);
+        System.out.println(NumberUtils.Int2ByteStr(h));
+        System.out.println(NumberUtils.Int2ByteStr(h >>> 16));
+        System.out.println(NumberUtils.Int2ByteStr(h ^ (h >>> 16)));
+        System.out.println((h ^ (h >>> 16)) & HASH_BITS);
+    }
 }
---
+++ src/main/java/com/coffee/util/NumberUtils.java
@@ -0,0 +1,8 @@
+package com.coffee.util;
+
+public class NumberUtils {
+
+    public static String Int2ByteStr(int n){
+        return Integer.toBinaryString(n);
+    }
+}

b97b007385c511b6091b58fbb94370aa41141139
makaijian <makaijian@huya.com>
2025-01-24 16:42:17 +0800

    [add] String stress test

--- src/main/java/com/coffee/png/testCase.java
+++ src/main/java/com/coffee/png/testCase.java
@@ -21,6 +21,33 @@ public class testCase {
             Math.round(5.5);
             System.out.println(Integer.MAX_VALUE+Integer.MAX_VALUE);
 
+
+
+            long startTime = System.currentTimeMillis();
+            String s = "start";
+            for (int i = 0; i < 10000; i++) {
+                s = s+i;
+
+            }
+            System.out.println("first call time:"+(System.currentTimeMillis()-startTime));
+
+
+            long tTime = System.currentTimeMillis();
+            StringBuffer sb1 = new StringBuffer("start");
+            for (int i = 0; i < 10000; i++) {
+                sb1.append(i);
+            }
+            System.out.println("third call time:"+(System.currentTimeMillis()-tTime));
+
+            long secondTime = System.currentTimeMillis();
+            StringBuilder sb = new StringBuilder("start");
+            for (int i = 0; i < 10000; i++) {
+                sb.append(i);
+            }
+            System.out.println("second call time:"+(System.currentTimeMillis()-secondTime));
+
+
+
 //            Thumbnails.of(new File("/Users/makaijian/Desktop/broadcast_banner_6.png"))
 //                    .outputFormat("jpg")
 //                    .scale(1.0)
